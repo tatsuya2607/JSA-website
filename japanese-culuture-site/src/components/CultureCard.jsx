881619af09089d3be6507915f76b95710ca1262c
Tatsuya-Ogawa26 <togawa@uco.edu>
2026-02-11 15:00:45 -0600

    I have learned about how to change color. Next is to learn to change the color of url

--- japanese-culuture-site/src/components/CultureCard.jsx
+++ japanese-culuture-site/src/components/CultureCard.jsx
@@ -5,11 +5,12 @@ function CultureCard({ id, title, description, image }) {
         <div className="culture-card">
             <h3>
                 <Link to={`/culture/${id}`}>
+                    {/* alt shows if the image fails to load, and also helps with accessibility */}
                     <img src={image} alt={title} className="card-image" />
-                    <h3>{title}</h3>
+                    <h3 className="title-card">{title}</h3>
                 </Link>
+                    <p className="description-card">{description}</p>
             </h3>
-            <p>{description}</p>
         </div>
     );
 }
