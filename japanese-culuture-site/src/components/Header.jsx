531fc0e99701ee7975e1304a840e51a18053dad1
Tatsuya-Ogawa26 <togawa@uco.edu>
2026-04-14 10:47:29 -0500

    initialized a repo

--- japanese-culuture-site/src/components/Header.jsx
+++ japanese-culuture-site/src/components/Header.jsx
@@ -1,13 +1,20 @@
 import { Link } from 'react-router-dom';
+import "../App.css";
+import sakura from "../assets/sakura-test.jpg";
 
 function Header() {
   return (
-    <header>
-      <h1>Japanese Culture</h1>
-      <nav>
-        <Link to="/">Home</Link> | {" "}
-        <Link to="/culture"> Culture</Link>
-      </nav>
+    <header
+      className="header"
+      style={{ backgroundImage: `url(${sakura})` }}
+    >
+      <div className="overlay">
+        <h1>Japanese Student Association</h1>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/culture">Culture</Link>
+        </nav>
+      </div>
     </header>
   );
 }
